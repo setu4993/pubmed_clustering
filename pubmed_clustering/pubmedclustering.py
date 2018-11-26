89ef0ef20de3719551843118989ec02fc7dcd607
Setu Shah <setu4993@yahoo.co.in>
2018-11-25 22:37:36 -0500

    Added methods to read labels, calculate purity

--- pubmed_clustering/pubmedclustering.py
+++ pubmed_clustering/pubmedclustering.py
@@ -28,15 +28,19 @@ class PubMedClustering:
 
         self.affinity_propagation = None
         self.clustering_results = None
+        self.clustering_results_dict = None
+
+        self.ground_truth = None
+        self.purity = None
 
     def run(self):
         self.documents, self.pmids = fetch_pubmed_documents(self.pubmed_ids, email=self.email, is_file=self.is_file)
         self.disease_words, self.metamap_json = documents_disease_list(self.documents)
-        self._create_tfidf_model()
-        self._calc_document_matrix()
-        self._clustering()
+        self.__create_tfidf_model()
+        self.__calc_document_matrix()
+        self.__clustering()
 
-    def _create_tfidf_model(self):
+    def __create_tfidf_model(self):
         logging.info('Creating dictionary')
         self.dictionary = Dictionary(self.disease_words)
         logging.info('Filtering low occurence words from dictionary')
@@ -47,7 +51,7 @@ class PubMedClustering:
         logging.info('Creating a TF-IDF model')
         self.tfidf_model = TfidfModel(self.corpus)
 
-    def _calc_document_matrix(self):
+    def __calc_document_matrix(self):
         logging.info('Calculating document matrix')
 
         self.document_matrix = np.zeros((len(self.disease_words), len(self.dictionary)))
@@ -59,9 +63,43 @@ class PubMedClustering:
                 self.document_matrix[i][dct_id] = score
         logging.info('Document matrix calculated')
 
-    def _clustering(self):
+    def __clustering(self):
         logging.info('Performing clustering on the document matrix')
         self.affinity_propagation = AffinityPropagation(damping=0.5, max_iter=500).fit(self.document_matrix)
         logging.info('Clustering performed')
         self.clustering_results = self.affinity_propagation.labels_
-        logging.info('Clustering results saved to class')
+        self.clustering_results_dict = {pmid: label for pmid, label in zip(self.pmids, self.clustering_results)}
+        self.total_clusters = len(self.affinity_propagation.cluster_centers_indices_)
+        logging.info('Clustering results saved to object')
+
+    def __original_labels(self, ground_truth, is_file=False):
+        if is_file:
+            doc_lines = open(ground_truth, 'r').readlines()
+            ground_truth = self.__read_labels(doc_lines)
+
+        if type(ground_truth) is dict:
+            self.ground_truth = ground_truth
+        else:
+            raise TypeError('Incorrect type of `ground_truth` variable')
+
+    def __read_labels(self, doc_lines):
+        categories_dict = {}
+        pmid_ground_truth = {}
+        i = 0
+        for line in doc_lines:
+            contents = line.strip().split('\t')
+            if contents[1] not in categories_dict:
+                categories_dict[contents[1]] = i
+                i += 1
+            pmid_ground_truth[contents[0]] = categories_dict[contents[1]]
+        return pmid_ground_truth
+
+    def __purity(self):
+        cluster_results = [{} for _ in range(self.total_clusters)]
+        for pmid, cluster in self.clustering_results_dict.items():
+            if self.ground_truth[pmid] not in cluster_results[cluster]:
+                cluster_results[cluster][self.ground_truth[pmid]] = 0
+            cluster_results[cluster][self.ground_truth[pmid]] += 1
+
+        cluster_max = [max(cluster_result.values()) for cluster_result in cluster_results]
+        self.purity = sum(cluster_max) / len(self.clustering_results_dict)
