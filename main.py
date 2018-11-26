907898f8f0b10035291d966c511bdbf5d500fb62
Setu Shah <setu4993@yahoo.co.in>
2018-11-25 23:47:09 -0500

    Updated to show results for new methods

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ logger = logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelnam
 
 
 if __name__ == '__main__':
-    clus = PubMedClustering(os.path.join('./data', 'pmids_gold_set_unlabeled.txt'), is_file=True)
+    clus = PubMedClustering(os.path.join('./data', 'pmids_gold_set_unlabeled.txt'),
+                            metamap='/opt/public_mm/bin/metamap16', email="This.Is.My@Email.org", is_file=True,
+                            labels=os.path.join('./data', 'pmids_gold_set_labeled.txt'), labels_is_file=True)
     clus.run()
-    print(clus.clustering_results)
+    print('Clustering results: ', clus.clustering_results)
+    print('Purity:', clus.purity)
+    print('F-measure:', clus.f_measure)
